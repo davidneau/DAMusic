d5e8d1fbb07442bc299d3ef734513b6d583579aa
david <davidneau49@gmail.com>
2025-01-05 22:02:29 +0100

    SW lire une video en arriere plan

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+const cacheName = 'pwa-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/votre-video.mp4',
+  '/styles.css',
+  '/script.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+        return cache.addAll(self.__WB_MANIFEST); // Workbox injecte les fichiers ici
+    })
+);
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+        })
+    );
+});
