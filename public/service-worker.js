5a00927621a8fead997640ccbae1854dd483be74
david <davidneau49@gmail.com>
2025-01-05 22:06:06 +0100

    SW lire une video en arriere plan

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,18 +7,26 @@ const filesToCache = [
   '/script.js'
 ];
 
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+        return cache.addAll(filesToCache);
+        })
+    );
+});
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-    caches.open('my-cache').then((cache) => {
-        return cache.addAll(self.__WB_MANIFEST); // Workbox injecte les fichiers ici
-    })
-);
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll(self.__WB_MANIFEST); // Workbox injecte les fichiers ici
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+            return response || fetch(event.request);
         })
     );
 });
